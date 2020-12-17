d1dc88cc9b24ab0c0ef61c321eb1ee36799c9675
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2020-12-17 09:06:30 +0100

    Fix body signature

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
-'use strict';
+"use strict";
 
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 module.exports.templateTags = [
     {
-        name: 'hmac',
-        displayName: 'HMAC',
-        description: 'Apply HMAC to a value',
+        name: "hmac",
+        displayName: "HMAC",
+        description: "Apply HMAC to a value",
         args: [
             {
-                displayName: 'Algorithm',
-                type: 'enum',
+                displayName: "Algorithm",
+                type: "enum",
                 options: [
-                    { displayName: 'MD5', value: 'md5' },
-                    { displayName: 'SHA1', value: 'sha1' },
-                    { displayName: 'SHA256', value: 'sha256' },
-                    { displayName: 'SHA512', value: 'sha512' }
-                ]
+                    { displayName: "MD5", value: "md5" },
+                    { displayName: "SHA1", value: "sha1" },
+                    { displayName: "SHA256", value: "sha256" },
+                    { displayName: "SHA512", value: "sha512" },
+                ],
             },
             {
-                displayName: 'Digest Encoding',
-                description: 'The encoding of the output',
-                type: 'enum',
+                displayName: "Digest Encoding",
+                description: "The encoding of the output",
+                type: "enum",
                 options: [
-                    { displayName: 'Hexadecimal', value: 'hex' },
-                    { displayName: 'Latin', value: 'latin1' },
-                    { displayName: 'Base64', value: 'base64' }
-                ]
+                    { displayName: "Hexadecimal", value: "hex" },
+                    { displayName: "Latin", value: "latin1" },
+                    { displayName: "Base64", value: "base64" },
+                ],
             },
             {
-                displayName: 'Key',
-                type: 'string',
-                placeholder: 'HMAC Secret Key'
+                displayName: "Key",
+                type: "string",
+                placeholder: "HMAC Secret Key",
             },
             {
-                displayName: 'Identifier',
-                type: 'string',
-                placeholder: 'HMAC Prefix'
-            }
+                displayName: "Identifier",
+                type: "string",
+                placeholder: "HMAC Prefix",
+            },
+            {
+                displayName: "bearer",
+                type: "string",
+                placeholder: "Optional Credential value",
+            },
         ],
-        async run(context, algorithm, encoding, key = '', identifier = '') {
+        async run(
+            context,
+            algorithm,
+            encoding,
+            key = "",
+            identifier = "",
+            bearer = undefined
+        ) {
             const { meta } = context;
             if (
-                encoding !== 'hex' &&
-                encoding !== 'latin1' &&
-                encoding !== 'base64'
+                encoding !== "hex" &&
+                encoding !== "latin1" &&
+                encoding !== "base64"
             ) {
                 throw new Error(
                     `Invalid encoding ${encoding}. Choices are hex, latin1, base64`
@@ -52,34 +64,50 @@ module.exports.templateTags = [
             }
 
             const hmac = crypto.createHmac(algorithm, key);
+            // console.log('bearer', bearer);
 
             const time = Date.now().toString();
+            // const time = '1585068666450';
+            // console.log('time', time);
             hmac.update(time);
-
             const request = await context.util.models.request.getById(
                 meta.requestId
             );
+            const url = await context.util.render(request.url);
+            // console.log('request', url, request);
             const httpMethod = request.method;
             hmac.update(httpMethod);
+            // console.log('httpMethod', httpMethod);
 
             // Regex source : https://tools.ietf.org/html/rfc3986#appendix-B
-            const match = request.url.match(
+            const match = url.match(
                 /^(([^:\/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/
             );
             let uri = match[5];
-            if (typeof match[6] === 'string') {
+            if (typeof match[6] === "string") {
                 uri += match[6];
             }
             hmac.update(uri);
+            // console.log('uri', match, uri);
 
             const body = request.body.text;
+            // console.log('body', body);
             if (body) {
-                const contentHash = crypto.createHash('md5');
-                contentHash.update(JSON.stringify(JSON.parse(body)));
-                hmac.update(contentHash.digest(encoding));
+                const realBody = await context.util.render(body);
+                // console.log('realBody', realBody);
+                const contentHash = crypto.createHash("md5");
+                const bodyString = JSON.stringify(JSON.parse(realBody));
+                contentHash.update(bodyString);
+                const bodyDigest = contentHash.digest(encoding).replace(/\s+/g,'')
+                hmac.update(bodyDigest);
+                // console.log('bodyString', bodyString);
+                // console.log('bodyDigest', bodyDigest);
             }
-
-            return `${identifier} ${time}:${hmac.digest(encoding)}`;
-        }
-    }
+            const result = hmac.digest(encoding);
+            // console.log('result', encoding , result);
+            return `${identifier}-${algorithm.toUpperCase()} ${
+                bearer ? `Bearer ${bearer} ` : ""
+            }Signature=${time}:${result}`;
+        },
+    },
 ];
